9c0086b0a9ca2802c68bc58709455582a71267a1
Syaiful-Maulana <59109706+Syaiful-Maulana@users.noreply.github.com>
2021-09-25 18:33:43 +0700

    Update prosesEditSewa.php

--- prosesEditSewa.php
+++ prosesEditSewa.php
@@ -1,5 +1,8 @@
 <?php
+//memasukkan fle koneksi didalam file prosesEditSewa.php
 include_once('koneksi.php');
+//$query menyiman hasil query
+// ketika berada di id yang dituju maka user dapat merubah data dari judul, penyewa, dan durasi
 $id = $_GET['id'];
 $judul = $_POST['judul'];
 $penyewa = $_POST['penyewa'];
