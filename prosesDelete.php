33b575025926d012a71c76b63b0d7fdb1179ebc5
Syaiful-Maulana <59109706+Syaiful-Maulana@users.noreply.github.com>
2021-09-25 18:35:14 +0700

    Update prosesDelete.php

--- prosesDelete.php
+++ prosesDelete.php
@@ -1,5 +1,7 @@
 <?php
+//memasukkan fle koneksi didalam file prosesDelete.php
 include_once('koneksi.php');
+// user akan menghapus data di table dimana id yang dituju di klik
 $id = $_GET['id'];
 
 mysqli_query($koneksi, "DELETE FROM idsewa WHERE id='$id'");
